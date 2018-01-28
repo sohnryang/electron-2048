c567a84975a534450a39c269fc46e3b1f2d55184
sohnryang <loop.infinitely@gmail.com>
2018-01-28 15:43:40 +0900

    fixed deps & added module for importing

---
+++ third-party/third-party.js
@@ -0,0 +1,13 @@
+import * from './game_manager';
+import * from './grid';
+import * from './html_actuator';
+import * from './keyboard_input_manager';
+import * form './local_storage_manager';
+
+module.exports = {
+    GameManager: GameManager,
+    Grid: Grid,
+    HTMLActuator: HTMLActuator,
+    KeyboardInputManager: KeyboardInputManager,
+    LocalStorageManager: LocalStorageManager
+}

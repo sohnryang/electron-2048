a7d06f354cab83e3b558cde5813cacc1be0edffd
sohnryang <loop.infinitely@gmail.com>
2018-01-28 15:44:41 +0900

    imported code

--- renderer.js
+++ renderer.js
@@ -1,1 +1,5 @@
-// a renderer.
+import * as third_party from 'third-party/third-party';
+
+window.requestAnimationFrame(() => {
+    new third_party.GameManager(4, third_party.KeyboardInputManager, third_party.HTMLActuator, third_party.LocalStorageManager);
+});
